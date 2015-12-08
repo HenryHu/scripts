0a6edbba8dd21d4e9b3e193f2132f1ec324d1b5b
Henry Hu <henry.hu.sh@gmail.com>
2015-12-07 19:54:28 -0500

    new environment

--- tempmon.pl
+++ tempmon.pl
@@ -1,4 +1,6 @@
 #! /usr/bin/perl
+
+$nogpu = 1;
 if ($ARGV[0] eq "-nogpu")
 {
     $nogpu = 1;
@@ -7,8 +9,13 @@ while (1)
 {
 	$c0 = `sysctl dev.cpu.0.temperature`;
 	$c1 = `sysctl dev.cpu.1.temperature`;
+	$c2 = `sysctl dev.cpu.2.temperature`;
+	$c3 = `sysctl dev.cpu.3.temperature`;
+	$c4 = `sysctl dev.cpu.4.temperature`;
+	$c5 = `sysctl dev.cpu.5.temperature`;
+	$c6 = `sysctl dev.cpu.6.temperature`;
+	$c7 = `sysctl dev.cpu.7.temperature`;
 	$z0 = `sysctl hw.acpi.thermal.tz0.temperature`;
-	$z1 = `sysctl hw.acpi.thermal.tz1.temperature`;
     if (!$nogpu)
     {
 	    $g0 = `nvidia-settings -q \[gpu:0\]/GPUCoreTemp -c :0`;
@@ -25,13 +32,25 @@ while (1)
 	$c1 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
 	# print $c1;
 	print "CPU 1: ".$1." C\n";
+	$c2 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
+	print "CPU 2: ".$1." C\n";
+	$c3 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
+	print "CPU 3: ".$1." C\n";
+	$c4 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
+	print "CPU 4: ".$1." C\n";
+	$c5 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
+	print "CPU 5: ".$1." C\n";
+	$c6 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
+	print "CPU 6: ".$1." C\n";
+	$c7 =~ /[^ ]*:\ *(\d+\.\d+)C$/;
+	print "CPU 7: ".$1." C\n";
 	#print $c1;
 	$_ = $z0;
 	/[^ ]+\s+([\d.]+)C/;
 	print "Thermal Zone 0: ".$1." C\n";
-	$_ = $z1;
-	/[^ ]+\s+([\d.]+)C$/;
-	print "Thermal Zone 1: ".$1." C\n";
+#	$_ = $z1;
+#	/[^ ]+\s+([\d.]+)C$/;
+#	print "Thermal Zone 1: ".$1." C\n";
     if (!$nogpu)
     {
 	    $g0 =~ /[^)]*\):\s*(.*)\./;
