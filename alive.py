95d08002171927855c7be1d008b5bd4424f39330
Henry Hu <henry.hu.sh@gmail.com>
2016-06-13 15:55:32 -0400

    pep8

--- alive.py
+++ alive.py
@@ -3,17 +3,22 @@
 import subprocess
 import time
 import logging
+
+
 def get_window_list(window_class):
     try:
-        output = subprocess.check_output('xdotool search --onlyvisible --class %s' % window_class, shell=True)
+        output = subprocess.check_output('xdotool search --onlyvisible --class %s' %
+                                         window_class, shell=True)
         return output.strip().split()
     except:
         return []
 
+
 def window_large_enough(window_id, width, height):
-    output = subprocess.check_output('xdotool getwindowgeometry --shell %s' % window_id, shell=True)
+    output = subprocess.check_output('xdotool getwindowgeometry --shell %s' % window_id,
+                                     shell=True)
     for line in output.strip().split('\n'):
-        if not '=' in line:
+        if '=' not in line:
             continue
         (name, val) = line.split('=')
         if name == 'WIDTH':
@@ -24,20 +29,25 @@ def window_large_enough(window_id, width, height):
                 return False
     return True
 
+
 def any_window_large_enough(window_list, width, height):
     for window_id in window_list:
         if window_large_enough(window_id, width, height):
             return True
     return False
 
+
 def delay_screensaver():
     logging.debug("delaying screen saver")
     subprocess.call('xscreensaver-command --deactivate', shell=True)
-    subprocess.call('qdbus org.freedesktop.ScreenSaver /ScreenSaver SimulateUserActivity', shell=True)
+    subprocess.call('qdbus org.freedesktop.ScreenSaver /ScreenSaver SimulateUserActivity',
+                    shell=True)
     subprocess.check_output('xset -dpms', shell=True)
+
 
 def restore_screensaver():
     subprocess.check_output('xset +dpms', shell=True)
+
 
 def get_proc_list(signature):
     try:
@@ -46,6 +56,7 @@ def get_proc_list(signature):
     except:
         return []
 
+
 def using_more_cpu(pid, percent):
     try:
         output = subprocess.check_output('ps -p %s -o %%cpu=' % pid, shell=True)
@@ -53,11 +64,13 @@ def using_more_cpu(pid, percent):
     except:
         return False
 
+
 def any_proc_using_cpu(proc_list, percent):
     for proc in proc_list:
         if using_more_cpu(proc, percent):
             return True
     return False
+
 
 def run_check():
     window_list = get_window_list('npviewer.bin') + get_window_list('pluginloader.exe')
@@ -71,6 +84,7 @@ def run_check():
     else:
         logging.debug("no window large enough")
         restore_screensaver()
+
 
 def run_loop(check_interval):
     while True:
