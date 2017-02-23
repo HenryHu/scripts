dc1f6ff309c4a6396d874fb7e6e355eac8d470d0
Henry Hu <henry.hu.sh@gmail.com>
2017-02-22 20:00:19 -0500

    better script

--- do_ssh.sh
+++ do_ssh.sh
@@ -15,18 +15,24 @@ scp $1 $2:otherkey.pub
 echo Check for existence
 if ssh $2 sudo test -e /home/$3/.ssh/authorized_keys; then
     echo Target file already exists.
-    ssh $2 "sudo sh -c \"cat otherkey.pub >> /home/$3/.ssh/authorized_keys\""
+    if ssh $2 sudo diff /home/$3/.ssh/authorized_keys otherkey.pub; then
+        echo "Target file matches new file."
+    else
+        ssh $2 "sudo sh -c \"cat otherkey.pub >> /home/$3/.ssh/authorized_keys\""
+    fi
     ssh $2 rm otherkey.pub
 else
     echo Create authorized_keys
-    ssh $2 sudo mkdir /home/$3/.ssh
+    if ssh $2 sudo test ! -e /home/$3/.ssh; then
+        ssh $2 sudo mkdir /home/$3/.ssh
+    fi
     ssh $2 sudo mv otherkey.pub /home/$3/.ssh/authorized_keys
-
-    echo Set permission \& owner
-    ssh $2 sudo chmod 700 /home/$3/.ssh
-    ssh $2 sudo chmod 644 /home/$3/.ssh/authorized_keys
-    ssh $2 sudo chown -R $3:$3 /home/$3/.ssh
 fi
+
+echo Set permission \& owner
+ssh $2 sudo chmod 700 /home/$3/.ssh
+ssh $2 sudo chmod 644 /home/$3/.ssh/authorized_keys
+ssh $2 sudo chown -R $3:$3 /home/$3/.ssh
 
 echo Finished.
 ssh $2 sudo ls -ld /home/$3/.ssh
