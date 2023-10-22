3108e38b1b10b30a7e95aa51a31015a41d5e0075
Henry Hu <henry.hu.sh@gmail.com>
2023-10-22 11:37:23 -0400

    read cpufreq from PCM

--- tmux_stat.py
+++ tmux_stat.py
@@ -13,7 +13,32 @@ def get_result_from_sysctl(node):
     return get_result_from_command(["sysctl", "-n", node])
 
 def get_result_from_bsdsensors(sensor):
-    return get_result_from_command(["bsdsensors", "-sensors", sensor])
+    #return get_result_from_command(["sudo", "bsdsensors", "-sensors", sensor])
+    return ""
+
+def get_max_cpu_freq_from_pcm():
+    subprocess.check_output(['pcm-core', '0.5', '-csv=/tmp/pcm.csv', '-i=1'])
+    period = 500
+    max_freq = 0
+    for line in open("/tmp/pcm.csv", "r"):
+        line = line.strip()
+        if line.startswith('Time elapsed'):
+            period = int(line.split(':')[1].strip().split(' ')[0])
+        if line.endswith(','):
+            parts = line.split(',')
+            cpu_num = parts[0]
+            if cpu_num == '*': continue
+            cycles = float(parts[3])
+            freq = cycles / period
+            max_freq = max(max_freq, freq)
+
+    return int(max_freq / 1000)
+
+def get_max_cpu_freq():
+    try:
+        return get_max_cpu_freq_from_pcm()
+    except:
+        return ''
 
 def get_cpu_temp():
     return get_result_from_sysctl("dev.cpu.0.temperature").rstrip('C')
@@ -26,7 +51,17 @@ def get_mb_temp():
 
 
 def get_gpu_temp():
-    return get_result_from_command(["nvidia-settings", "-q", "0/GPUCoreTemp", "-t"])
+    try:
+        return get_result_from_command(["nvidia-settings", "-q", "0/GPUCoreTemp", "-t"])
+    except:
+        return None
+
+def get_freq_str(name, freq):
+    if not freq:
+        return ''
+    if freq > 1000:
+        return "%s: %.2fG" % (name, freq / 1000.0)
+    return "%s: %sM" % (name, freq)
 
 def get_temp_color(temp):
     if temp >= 90:
@@ -47,6 +82,8 @@ def get_temp_color(temp):
         return "green"
 
 def get_temp_str(name, temp):
+    if not temp:
+        return ''
     try:
         temp_val = float(temp)
         temp_color = get_temp_color(temp_val)
@@ -60,13 +97,15 @@ def get_time_str():
     return "#[default]#[fg=green]%s #[fg=blue]%s#[default]" % (curdate, curtime)
 
 
+cpu_freq = get_max_cpu_freq()
 cpu_temp = get_cpu_temp()
 mb_temp = get_mb_temp()
 gpu_temp = get_gpu_temp()
 
+cpu_freq_str = get_freq_str("CPU", cpu_freq)
 cpu_temp_str = get_temp_str("CPU", cpu_temp)
 mb_temp_str = get_temp_str("MB", mb_temp)
 gpu_temp_str = get_temp_str("GPU", gpu_temp)
 time_str = get_time_str()
-print("%s %s %s %s" % (cpu_temp_str, mb_temp_str, gpu_temp_str, time_str))
+print("%s %s" % (' '.join(filter(lambda x: x, [cpu_freq_str, cpu_temp_str, mb_temp_str, gpu_temp_str])), time_str))
 
